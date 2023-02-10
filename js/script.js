e6485f4e7d02e167894f47cf6fad05ca704ce82c
jdrcabrera <jd.rcabrera@gmail.com>
2023-02-10 18:12:54 -0500

    Carousel added

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function changingStates() {
+    const menuIcon = document.querySelector('.menuIcon')
+    menuIcon.classList.toggle('active')
+    const showHeader = document.getElementById('navbar')
+    showHeader.classList.toggle('active')
+    const controls = document.querySelector('.controls')
+    controls.classList.toggle('active')
+}
+
+const slideShow = document.querySelector('#slideShow')
+const slides = slideShow.getElementsByTagName('img')
+
+
+
+var index = 0;
+function nextSlide() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+function prevSlide() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+const slideShowText = document.getElementById('slideShowText')
+const slideText = slideShowText.getElementsByTagName('div')
+var indexText = 0;
+function nextSlideText() {
+    slideText[indexText].classList.remove('active');
+    indexText = (indexText + 1) % slideText.length;
+    slideText[index].classList.add('active');
+}
+function prevSlideText() {
+    slideText[indexText].classList.remove('active');
+    indexText = (indexTex - 1 + indexText.length) % slideText.length;
+    slideText[index].classList.add('active');
+}
